6d418454935ff53943ceed1e9aa7cec47104d85e
Sarah Yoon <syoon2@stuy.edu>
2017-01-07 22:52:42 -0500

    okay now bug is fixed

--- utils/auth.py
+++ utils/auth.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, url_for, session, redirect
 import hashlib, sqlite3
 
-f = "database.db"
+f = "data/database.db"
 
 def register(username, password):
     if (username=="" or password==""): return "Please fill in the username and password fields"
